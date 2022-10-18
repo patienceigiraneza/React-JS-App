203934bad3ed8dc57a39df5285fbc2dd0ab3783f
patienceigiraneza <patienceigiraneza2@gmail.com>
2022-10-18 16:30:11 +0200

    1. learn on app serve

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+import Header from "./components/Header";
 
 function App() {
+  const fname = 'Patience';
+  const lname = 'Igiraneza';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header title="My website title"/>
+      <h1> Welcome to my first React JS app, <br></br>   </h1>
+      <h2> React is dreams maker of {fname} {lname}. </h2>
     </div>
   );
 }
+
+// class App extends React.Component{
+//   render(){
+//     return <h1> Hello from the class </h1>
+//   }
+// }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <header>
+        <h2>{props.title}</h2>
+        My Header divison
+
+    </header>
+  )
+}
+
+export default Header
